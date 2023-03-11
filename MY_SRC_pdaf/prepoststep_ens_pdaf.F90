f50b822bebb454dd4dc116e602f77a1f2295d3af
Lars Nerger <lars.nerger@awi.de>
2023-03-11 13:15:31 +0100

    Revise for more efficient computation of ensemble spread.

--- MY_SRC_pdaf/prepoststep_ens_pdaf.F90
+++ MY_SRC_pdaf/prepoststep_ens_pdaf.F90
@@ -76,7 +76,7 @@ subroutine prepoststep_ens_pdaf(step, dim_p, dim_ens, dim_ens_p, dim_obs_p, &
   integer, allocatable :: dimfield(:)  ! Global field dimensions
   real, allocatable :: rmse_est(:)     ! Global estimated RMS errors (ensemble standard deviations)
   logical :: inirestart                ! Whether prepoststep is called first time with ensemble restart
-  
+  real :: dimfield_inv
 
   
 ! **********************
@@ -144,177 +144,192 @@ subroutine prepoststep_ens_pdaf(step, dim_p, dim_ens, dim_ens_p, dim_obs_p, &
   enddo
   
 
-! ************************************************************
-! *** Compute RMS errors according to sampled covar matrix ***
-! ************************************************************
+! ******************************************************************
+! *** Compute ensemble standard deviation (estimated RMS errors) ***
+! ******************************************************************
 
-  allocate(rmse_est_p(n_fields))
-  allocate(rmse_est(n_fields))
-  allocate(dimfield_p(n_fields))
-  allocate(dimfield(n_fields))
-  rmse_est_p  = 0.0_8
+  if (.not. inirestart) then
 
-  dimfield_p(:) = sfields(:)%dim
-  call MPI_Allreduce(dimfield_p, dimfield, n_fields, MPI_INTEGER, MPI_SUM, COMM_filter, MPIerr)
+     allocate(rmse_est_p(n_fields))
+     allocate(dimfield_p(n_fields))
+     if (mype==0) allocate(rmse_est(n_fields))
+     if (mype==0) allocate(dimfield(n_fields))
 
-  ! total estimated mean-square error per field per process
-  do j = 1, n_fields
-
-     do i = 1+sfields(j)%off, sfields(j)%dim+sfields(j)%off
-        rmse_est_p(j) = rmse_est_p(j) + state_tmp(i)
+     ! Total sum of variance per field per process
+     rmse_est_p  = 0.0_8
+     do j = 1, n_fields
+        do i = 1+sfields(j)%off, sfields(j)%dim+sfields(j)%off
+           rmse_est_p(j) = rmse_est_p(j) + state_tmp(i)
+        enddo
      enddo
-     rmse_est_p(j) = rmse_est_p(j) / real(dimfield(j), 8)
 
-  enddo
+     ! Global sum of variance per field
+     call MPI_Reduce (rmse_est_p, rmse_est, n_fields, MPI_DOUBLE_PRECISION, MPI_SUM, &
+          0, COMM_filter, MPIerr)
 
-  ! Global sum of mean squared errors
-  call MPI_Allreduce (rmse_est_p, rmse_est, n_fields, MPI_DOUBLE_PRECISION, MPI_SUM, &
-       COMM_filter, MPIerr)
+     ! Get global field dimensions
+     dimfield_p(:) = sfields(:)%dim
+     call MPI_Reduce(dimfield_p, dimfield, n_fields, MPI_INTEGER, MPI_SUM, 0, COMM_filter, MPIerr)
 
-  ! Get global RMSE
-  rmse_est = sqrt(rmse_est)
+     ! Total estimated ensemble standard deviation per field per process
+     if (mype == 0) then
+        do j = 1, n_fields
+           dimfield_inv = 1.0 / real(dimfield(j), 8)
 
+           rmse_est(j) = rmse_est(j) * dimfield_inv
+        end do
 
-! *****************
-! *** Screen IO ***
-! *****************
+        ! Get global STDDEV
+        rmse_est = sqrt(rmse_est)
+     end if
 
-  ! Output RMS errors given by sampled covar matrix
-   if (mype == 0 .and. .not. inirestart) then
-      write (*, '(a,6x,a)') 'NEMO-PDAF', 'RMS errors according to sample variance'
-      do i = 1, n_fields
-         write (*,'(a,4x,a8,4x,a10,2x,es12.4)') &
-              'NEMO-PDAF', 'RMSE-'//forana, trim(sfields(i)%variable), rmse_est(i)
-      end do
-   end if
+     ! Output ensemble standard deviations
+     if (mype == 0 .and. .not. inirestart) then
+        write (*, '(a,6x,a)') 'NEMO-PDAF', 'Ensemble standard deviation (estimated RMS error)'
+        do i = 1, n_fields
+           write (*,'(a,4x,a8,4x,a10,2x,es12.4)') &
+                'NEMO-PDAF', 'RMSE-'//forana, trim(sfields(i)%variable), rmse_est(i)
+        end do
+     end if
+
+  end if
 
 
 ! *******************
 ! *** File output ***
 ! *******************
 
-   ! Set time string
-   WRITE(ndastp_str,'(I8.8)') ndastp
+  ! Set time string
+  WRITE(ndastp_str,'(I8.8)') ndastp
 
-   ! *** Write variance into nc file ***
+  ! *** Write variance into nc file ***
 
-   writevar: if (trim(save_var_time)=='ana' .or. trim(save_var_time)=='fcst' &
-        .or. trim(save_var_time)=='both') then
+  writevar: if (trim(save_var_time)=='ana' .or. trim(save_var_time)=='fcst' &
+       .or. trim(save_var_time)=='both') then
 
-      if (writestep_var==1) then
+     if (writestep_var==1) then
 
-         if (mype == 0 .and. .not. inirestart) then
-            if (forana=='for') then
-               write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance before analysis step'
-            elseif (forana=='ana') then
-               if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance after analysis'
-            else if (.not. (ens_restart)) then
-               if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write initial variance file'
-            end if
-         end if
+        if (mype == 0 .and. .not. inirestart) then
+           if (forana=='for') then
+              write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance before analysis step'
+           elseif (forana=='ana') then
+              if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance after analysis'
+           else if (.not. (ens_restart)) then
+              if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write initial variance file'
+           end if
+        end if
 
-         if (save_var_time=='both' .and. forana/='ini') then
-            nsteps = 2
-         else
-            nsteps = 1
-         end if
+        if (save_var_time=='both' .and. forana/='ini') then
+           nsteps = 2
+        else
+           nsteps = 1
+        end if
 
-         titleVar='Ensemble variance'
+        titleVar='Ensemble variance'
 
-         if (forana/='ini') then
-            call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'.nc', &
-                 titleVar, 1.0, nsteps, writestep_var, 0)
-         else if (.not. (ens_restart)) then
-            call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'_ini.nc', &
-                 titleVar, 1.0, nsteps, writestep_var, 0)
-         end if
-         if (forana/='ini' .and. trim(save_var_time)=='both') writestep_var = 2
+        if (forana/='ini') then
+           call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'.nc', &
+                titleVar, 1.0, nsteps, writestep_var, 0)
+        else if (.not. (ens_restart)) then
+           call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'_ini.nc', &
+                titleVar, 1.0, nsteps, writestep_var, 0)
+        end if
+        if (forana/='ini' .and. trim(save_var_time)=='both') writestep_var = 2
 
-      elseif (writestep_var>1 .and. (trim(save_var_time)=='ana' .or. trim(save_var_time)=='both')) then
+     elseif (writestep_var>1 .and. (trim(save_var_time)=='ana' .or. trim(save_var_time)=='both')) then
+        
+        if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance after analysis step'
 
-         if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write variance after analysis step'
+        call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'.nc', &
+             titleVar, 1.0, 2, writestep_var, 0)
 
-         call write_field_mv(state_tmp, trim(file_PDAF_variance)//'_'//trim(ndastp_str)//'.nc', &
-              titleVar, 1.0, 2, writestep_var, 0)
-
-         writestep_var = 1
-      end if
-   end if writevar
-
-
-   ! *** Write state into nc file ***
-   if (save_State .and. (.not. (ens_restart .and. forana=='ini')) ) then
-
-      ! Store state in state_tmp to avoid changing state_p
-      state_tmp = state_p
-
-      titleState = 'Ensemble mean state'
-
-      ! Write state file for viewing
-      if (forana=='for') then
-         if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean before analysis step'
-         writestep_state = 1
-      elseif (forana=='ana') then
-          if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean after analysis step'
-         writestep_state = 2
-      else
-         if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean at initial time'
-         writestep_state = 1
-      end if
-
-      ! Write forecast and analysis into the same file
-      if (forana/='ini') then
-         call write_field_mv(state_tmp, trim(file_PDAF_state)//'_'//trim(ndastp_str)//'.nc', titleState, 1.0, 2, writestep_state, 1)
-      else
-         call write_field_mv(state_tmp, trim(file_PDAF_state)//'_'//trim(ndastp_str)//'_ini.nc', titleState, 1.0, 1, writestep_state, 1)
-      end if
-   endif
+        writestep_var = 1
+     end if
+  end if writevar
 
 
-   ! *** Write state into nc file ***
-   if (save_ens_sngl .and. (.not. (ens_restart .and. forana=='ini')) ) then
+  ! *** Write state into nc file ***
+  writestate: if (save_State .and. (.not. (ens_restart .and. forana=='ini')) ) then
 
-      do iens = 1, dim_ens
+     ! Store state in state_tmp to avoid changing state_p
+     state_tmp = state_p
 
-         write(ensstr,'(i3.3)') iens
+     titleState = 'Ensemble mean state'
 
-         ! Store state in state_tmp to avoid changing state_p
-         state_tmp = ens_p(:,iens)
+     ! Write state file for viewing
+     if (forana=='for') then
+        if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean before analysis step'
+        writestep_state = 1
+     elseif (forana=='ana') then
+        if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean after analysis step'
+        writestep_state = 2
+     else
+        if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble mean at initial time'
+        writestep_state = 1
+     end if
 
-         titleState = 'Ensemble state vector'
+     ! Write forecast and analysis into the same file
+     if (forana/='ini') then
+        call write_field_mv(state_tmp, trim(file_PDAF_state)//'_'//trim(ndastp_str)//'.nc', &
+             titleState, 1.0, 2, writestep_state, 1)
+     else
+        call write_field_mv(state_tmp, trim(file_PDAF_state)//'_'//trim(ndastp_str)//'_ini.nc', &
+             titleState, 1.0, 1, writestep_state, 1)
+     end if
+  endif writestate
 
-         ! Write state file for viewing
-         if (iens==1) then
-            if (forana=='for') then
-               if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state before analysis step'
-               writestep_state = 1
-            elseif (forana=='ana') then
-               if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state after analysis step'
-               writestep_state = 2
-            else
-               if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state at initial time'
-               writestep_state = 1
-            end if
-         end if
+
+  ! *** Write chlorophyll ensemble into nc files ***
+  savens: if (save_ens_sngl .and. (.not. (ens_restart .and. forana=='ini')) ) then
+
+     do iens = 1, dim_ens
+
+        write(ensstr,'(i3.3)') iens
+
+        ! Store state in state_tmp to avoid changing state_p
+        state_tmp = ens_p(:,iens)
+
+        titleState = 'Ensemble state vector'
+
+        ! Write state file for viewing
+        if (iens==1) then
+           if (forana=='for') then
+              if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state before analysis step'
+              writestep_state = 1
+           elseif (forana=='ana') then
+              if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state after analysis step'
+              writestep_state = 2
+           else
+              if (mype == 0) write (*,'(a,5x,a)') 'NEMO-PDAF', '--- Write ensemble state at initial time'
+              writestep_state = 1
+           end if
+        end if
 
          ! Write forecast and analysis into the same file
-         if (forana/='ini') then
-            call write_field_sngl(state_tmp, trim(file_PDAF_state)//'_'//trim(sfields(id_chl)%variable)//'_'//trim(ndastp_str)//'_'//ensstr//'.nc', titleState, 1.0, 2, writestep_state, 1, id_chl)
-         else
-            call write_field_sngl(state_tmp, trim(file_PDAF_state)//'_'//trim(sfields(id_chl)%variable)//'_'//trim(ndastp_str)//'_'//ensstr//'_ini.nc', titleState, 1.0, 1, writestep_state, 1, id_chl)
-         end if
-      end do
-   end if
+        if (forana/='ini') then
+           call write_field_sngl(state_tmp, &
+                trim(file_PDAF_state)//'_'//trim(sfields(id_chl)%variable)//'_'//trim(ndastp_str)//'_'//ensstr//'.nc', &
+                titleState, 1.0, 2, writestep_state, 1, id_chl)
+        else
+           call write_field_sngl(state_tmp, &
+                trim(file_PDAF_state)//'_'//trim(sfields(id_chl)%variable)//'_'//trim(ndastp_str)//'_'//ensstr//'_ini.nc', &
+                titleState, 1.0, 1, writestep_state, 1, id_chl)
+        end if
+     end do
+  end if savens
 
 
 ! ********************
 ! *** finishing up ***
 ! ********************
 
-   deallocate(state_tmp) 
-   deallocate(rmse_est_p, rmse_est, dimfield_p, dimfield)
+  deallocate(state_tmp) 
 
-   firsttime = .false.
+  if (.not. inirestart) then
+     deallocate(rmse_est_p, dimfield_p)
+     if (mype==0) deallocate(rmse_est, dimfield)
+  end if
 
+  firsttime = .false.
 
 end subroutine prepoststep_ens_pdaf
