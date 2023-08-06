2d26d44e95a254cdbafd1a8654309645e5341723
sovlie <hbxsovli@glogin8.usr.hlrn.de>
2023-08-06 18:17:51 +0200

    Change write statement to be not hard-coded anymore

--- TOOLS/plot/plot_rmse_3exp.py
+++ TOOLS/plot/plot_rmse_3exp.py
@@ -4,18 +4,23 @@ import numpy as bp
 from plot_lib import *
 
 # CONFIGURATION PART
-fname = 'rmse_log_chl_ba.png'
-plot_title = 'RMSe CHL (log)'
-nc_folder = '../rmse/netcdfs/' # if not in a different path just just empty string ('')
-file_free = nc_folder+'rms_chl_ba_free_N30_log.nc';
-file1 = nc_folder+'rms_chl_ba_chl_N30_log.nc'; #chl_N30
-file2 = nc_folder+'rms_chl_ba_sstL3_N30_log.nc'; #sstL3
-file3 = nc_folder+'rms_chl_ba_chl+sstL3_N30_log.nc' #chl+sstL3
+fname = 'rmse_sw_chl_ba.png'
+plot_title = 'RMSe SST'
+nc_folder = '../../../plots_copy/rmse/netcdfs/' # if not in a different path just just empty string ('')
+file_free = nc_folder+'rms_sst_L3S_ba_free_N30.nc';
+file1 = nc_folder+'rms_sst_L3S_ba_chl_N30.nc'; #chl_N30
+file2 = nc_folder+'rms_sst_L3S_ba_sstL3_N30.nc'; #sstL3
+file3 = nc_folder+'rms_sst_L3S_ba_chl+sstL3_N30.nc' #chl+sstL3
+#file_free = nc_folder+'rms_chl_ba_free_N30_log.nc';
+#file1 = nc_folder+'rms_chl_ba_chl_strongly_N30_log.nc'; #chl_N30
+#file2 = nc_folder+'rms_chl_ba_sstL3_strongly_N30_log.nc'; #sstL3
+#file3 = nc_folder+'rms_chl_ba_chl_weakly_sstL3_strongly_N30_log.nc' #chl+sstL3
 plot_ana = 0 # whether to plot analysis step
 operational = 1 # 1 for operational, 0 otherwise
-
+save = 0 #whether to save plot 
 # END CONFIGURATION
 
+print(plot_title)
 # read rmse netcdfs
 ncfile_free=nc.Dataset(file_free);
 ncfile1=nc.Dataset(file1);
@@ -48,22 +53,22 @@ for i in range(len(allmonths)):
 fig, ax = plt.subplots(figsize=(8,3))
 if operational==1:
    plt.plot(allday_free, rmse_free[0,:], 'b', label='FREE')
-   plt.plot(allday_assim, rmse1[0,:], '#bbbb00', label='CHL_DA')
-   plt.plot(allday_assim, rmse2[0,:], 'g', label='SST_DA')
-   plt.plot(allday_assim, rmse3[0,:], 'k', label='CHL+SST_DA')
+   plt.plot(allday_assim, rmse1[0,:], '#bbbb00', label='CHL strongly')
+   plt.plot(allday_assim, rmse2[0,:], 'g', label='SST strongly')
+   plt.plot(allday_assim, rmse3[0,:], 'k', label='CHL weakly + SST strongly')
    if plot_ana == 1:
-     plt.plot(allday_assim, rmse1[1,:], '#bbbb00',linestyle='--', label='CHL_DA')
-     plt.plot(allday_assim, rmse2[1,:], 'g--', label='SST_DA')
-     plt.plot(allday_assim, rmse3[1,:], 'k--', label='CHL+SST_DA')
+     plt.plot(allday_assim, rmse1[1,:], '#bbbb00',linestyle='--', label='CHL strongly')
+     plt.plot(allday_assim, rmse2[1,:], 'g--', label='SST strongly')
+     plt.plot(allday_assim, rmse3[1,:], 'k--', label='CHL weakly + SST strongly')
 else:
    plt.plot(allday_free, rmse_free[1,:], 'b', label='FREE')
-   plt.plot(allday_assim, rmse1[1,:], '#bbbb00', label='CHL_DA')
-   plt.plot(allday_assim, rmse2[1,:], 'g', label='SST_DA')
-   plt.plot(allday_assim, rmse3[1,:], 'k', label='CHL+SST_DA')
+   plt.plot(allday_assim, rmse1[1,:], '#bbbb00', label='CHL strongly')
+   plt.plot(allday_assim, rmse2[1,:], 'g', label='SST strongly')
+   plt.plot(allday_assim, rmse3[1,:], 'k', label='CHL weakly + SST strongly')
    if plot_ana == 1:
-     plt.plot(allday_assim, rmse1[2,:], '#bbbb00', linestyle='--', label='CHL_DA')
-     plt.plot(allday_assim, rmse2[2,:], 'g--', label='SST_DA')
-     plt.plot(allday_assim, rmse3[2,:], 'k--', label='CHL+SST_DA')
+     plt.plot(allday_assim, rmse1[2,:], '#bbbb00', linestyle='--', label='CHL strongly')
+     plt.plot(allday_assim, rmse2[2,:], 'g--', label='SST strongly')
+     plt.plot(allday_assim, rmse3[2,:], 'k--', label='CHL weakly + SST strongly')
 
 plt.title(plot_title)
 #plt.ylabel('RMSe ($^oC$)', fontsize=16)
@@ -77,14 +82,16 @@ ax.xaxis.set_major_formatter(ticker.NullFormatter())
 ax.tick_params(axis='x', which='minor', tick1On=False, tick2On=False)
 ax.xaxis.set_minor_formatter(ticker.FixedFormatter(months_str))
 
-
+assim1 = extractBetween(file1,'ba_', '_N30')
+assim2 = extractBetween(file2,'ba_', '_N30')
+assim3 = extractBetween(file3,'ba_', '_N30')
 # print RMSE mean to screen 
 print '----- MEAN RMSE ------'
 print 'Free = ', np.mean(rmse_free)
 print '_________ Forecast __________'
-print 'chl_N30 = ', np.mean(rmse1[0,:])
-print 'sstL3_N30 = ', np.mean(rmse2[0,:])
-print 'chl+sstL3_N30 = ', np.mean(rmse3[0,:])
+print assim1,' = ', np.mean(rmse1[0,:])
+print assim2, ' = ', np.mean(rmse2[0,:])
+print assim3, ' = ', np.mean(rmse3[0,:])
 if plot_ana == 1: 
   print '_________ Analysis __________'
   print 'chl_N30 = ', np.mean(rmse1[1,:])
@@ -92,5 +99,6 @@ if plot_ana == 1:
   print 'chl+sstL3_N30 = ', np.mean(rmse3[1,:])
 
 #plt.show()
-plt.savefig(fname, dpi=200)
+if save == 1: 
+  plt.savefig(fname, dpi=200)
 
